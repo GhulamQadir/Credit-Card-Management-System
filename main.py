da4a08d1ec08df5b39464db3d207ad3bd19220c1
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2025-09-09 11:01:01 +0500

    test credit cards list

--- main.py
+++ main.py
@@ -3,14 +3,48 @@ from credit_card_list.CC_List import CreditCardList
 
 
 def main():
-    cc = CreditCard("Ali", "Meezan Bank", "323 989 323 7827", 4000)
+    # Create a CreditCard instance for Ali with a limit of 4000
+    cc = CreditCard("Ali", "Meezan Bank", "323 989 323 782 665", 4000)
+
+    # Create an empty CreditCardList object
     cards_list = CreditCardList()
-    cards_list.add(cc)
-    cards_list.add(CreditCard("Shahid", "HBL", "173691263", 8000))
-    cards_list.add(CreditCard("Umer", "Faysal Bank", "173691263", 12000))
-    # cards_list.add_collection(cc,CreditCard("Shahid", "HBL", "173691263", 8000))
+    # Append the previously created CreditCard (Ali) to the list
+    cards_list.append(cc)
+
+    # Extend the list with multiple new CreditCard instances
+    cards_list.extend(
+        [
+            CreditCard("Shahid", "HBL", "173 069 126 333 009", 8000),
+            CreditCard("Umer", "Faysal Bank", "887 223 173 691 263", 12000),
+            CreditCard("Umer", "Meezan Bank", "240 349 232 778 887", 12000),
+        ]
+    )
     print(cards_list)
-    cards_list.insert_card(2, CreditCard("Sameer", "UBL", "243423423", 9000))
+
+    # Get the index of the card with number "1730691263" between positions -80 and 10
+    # get_index = cards_list.index("173 069 1263", -80, 10)
+    # print(get_index)
+
+    # Slice the cards_list from index 1 to 9 (like normal Python list slicing)
+    sliced_list = cards_list[1:9]
+    print("sliced_list: ", sliced_list)
+
+    # Remove the CreditCard with number "173691263" from the list
+    cards_list.remove("323 989 323 782 665")
+    print(cards_list)
+
+    # Insert a new CreditCard (Zahid) at index 1
+    cards_list.insert(
+        1, CreditCard("Zahid", "Bank Alfalah", "323 989 323 213 817", 4000)
+    )
+    print(cards_list)
+
+    # Delete the CreditCard at index 2
+    cards_list.delete(2)
+    print(cards_list)
+
+    # Remove the last CreditCard from the list (pop)
+    cards_list.pop()
     print(cards_list)
 
 
